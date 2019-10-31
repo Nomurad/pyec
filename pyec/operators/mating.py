5ee417084d2a105427ea67e9b9ca9c1568728d81
seiya nomura <nomurad@outlook.jp>
2019-10-31 19:26:14 +0900

    add Mating

--- pyec/operators/mating.py
+++ pyec/operators/mating.py
@@ -6,6 +6,39 @@ from .selection import TournamentSelection, TournamentSelectionStrict
 from .crossover import BlendCrossover, SimulatedBinaryCrossover
 from .mutation import PolynomialMutation
 
+class MatingError(Exception):
+    pass 
+
+class Mating(object):
+    
+    def __init__(self, crossover, mutation, pool:Pool):
+        self._crossover = crossover
+        self._mutation = mutation
+        self._pool = pool
+        self._parents = []
+        self._stored = []
+
+    def __call__(self, parents=None):
+        if (parents is None) and (self._parents is None):
+            raise MatingError("You should set parents.")
+        if (parents is None) and (self._parents is not None):
+            parents = self._parents
+
+        self._stored = []
+
+        parent_genomes = [indiv.get_genome() for indiv in parents]
+        child_genomes = self._crossover(parent_genomes)
+        
+        for child_genome in child_genomes:
+            child_genome = self._mutation(child_genome) #一定確率で突然変異
+            child_indiv = self._pool(child_genome)
+            self._stored.append(child_indiv)
+        
+        return self._stored
+
+    def Set_parents(self, parents):
+        self._parents = parents
+
 class PartialMatingIterator(object):
     ''' MatingIteratorの部分適用オブジェクト
     '''
