38afe9d7daac1ddcd89763c75a98187542c21a31
seiya nomura <nomurad@outlook.jp>
2020-06-02 07:19:01 +0900

    add  pareto solution picker

--- pyec/operators/sorting.py
+++ pyec/operators/sorting.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np 
 
 from ..base.population import Population
@@ -55,6 +57,38 @@ class NonDominatedSort(object):
             num_dominated -= np.sum(mask & is_dominated, axis=(1,))
 
         raise NonDominatedSortError("Error: reached the end of function")
+
+    def output_pareto(self, population:Population):
+        popsize = len(population)
+
+        is_dominated = np.empty((popsize, popsize), dtype=np.bool)
+        num_dominated = np.zeros(popsize, dtype=np.int64)
+        mask = np.empty(popsize, dtype=np.bool)
+        rank = np.zeros(popsize, dtype=np.int64)
+
+        front = []
+        # for i in range(popsize):
+        #     for j in range(popsize):
+        for i, j in itertools.product(range(popsize), range(popsize)):
+                # if i == j:
+                #     continue
+                #iがjに優越されている -> True
+                dom = population[j].dominate(population[i])
+                is_dominated[i,j] = (i!= j) and dom
+
+        #iを優越する個体の数
+        is_dominated.sum(axis=(1,), out=num_dominated)
+
+        for i in range(popsize):
+            if num_dominated[i] == 0:
+                front.append(population[i])
+
+            # is_rank_ditermined = not(rank[i] or num_dominated[i])
+            # mask[i] = is_rank_ditermined
+            # if is_rank_ditermined:
+            #     # rank[i] = r + 1
+            #     front.append(population[i])
+        return front
 
     def feasible_sort(self, population:Population, return_rank=False):
         popsize = len(population)
