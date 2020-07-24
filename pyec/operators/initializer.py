cac8427d841cd41a6af14926eb67fcd19603580f
seiya nomura <nomurad@outlook.jp>
2020-07-24 16:09:53 +0900

    random.rand -> random.uniform

--- pyec/operators/initializer.py
+++ pyec/operators/initializer.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 class UniformInitializer(object):
-    ''' [0, 1)の範囲の一様乱数による実数配列を返す '''
+    ''' [0, 1]の範囲の一様乱数による実数配列を返す '''
 
     def __init__(self, size):
         self._size = size
 
     def __call__(self):
-        return np.random.rand(self._size)
+        return np.random.uniform(size=self._size)
