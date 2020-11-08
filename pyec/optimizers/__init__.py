3941c430ce95cd0f9aded6e1dbe8577945ef3198
seiya nomura <nomurad@outlook.jp>
2020-11-08 23:57:23 +0900

    update

--- pyec/optimizers/__init__.py
+++ pyec/optimizers/__init__.py
@@ -1,4 +1,6 @@
 #from .moead import *
+from ..base.indiv import Individual
+from ..base.population import Population
 from abc import ABC, ABCMeta, abstractmethod
 
 class OptimizerError(Exception):
@@ -10,10 +12,18 @@ class Optimizer(ABC):
         self.popsize = popsize 
         self.n_obj = n_obj
 
-    def get_new_offspring(self):
+    def get_new_generation(self) -> Population:
         pass
 
     @abstractmethod
-    def get_offspring(self):
+    def get_offspring(self) -> Individual:
         pass 
+
+    def calc_fitness(self, population):
+        pass
+
+    def init_normalize(self, is_normalize: bool, option="unhold"):
+        self.normalize = is_normalize 
+        self.normalize_option = option 
+
 
