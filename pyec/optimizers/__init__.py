3f52ddb8c1d949dcdf924d324c17a84c198d0173
seiya nomura <nomurad@outlook.jp>
2020-11-08 21:12:24 +0900

    add optimizer abstract class

--- pyec/optimizers/__init__.py
+++ pyec/optimizers/__init__.py
@@ -1,4 +1,19 @@
 #from .moead import *
+from abc import ABC, ABCMeta, abstractmethod
 
 class OptimizerError(Exception):
     pass
+
+# class Optimizer(metaclass=ABCMeta):
+class Optimizer(ABC):
+    def __init__(self, popsize: int, n_obj: int):
+        self.popsize = popsize 
+        self.n_obj = n_obj
+
+    def get_new_offspring(self):
+        pass
+
+    @abstractmethod
+    def get_offspring(self):
+        pass 
+
