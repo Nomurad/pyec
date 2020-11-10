181cf38add73672cbbf51b36fcd386c6ef72935b
nomurad <nomura-seiya@ed.tmu.ac.jp>
2020-11-10 20:28:12 +0900

    modify abstractmethod's args

--- pyec/optimizers/__init__.py
+++ pyec/optimizers/__init__.py
@@ -1,22 +1,30 @@
-#from .moead import *
+from itertools import chain
+from types import FunctionType
+
 from ..base.indiv import Individual
 from ..base.population import Population
 from abc import ABC, ABCMeta, abstractmethod
 
+
 class OptimizerError(Exception):
     pass
 
-# class Optimizer(metaclass=ABCMeta):
+
 class Optimizer(ABC):
     def __init__(self, popsize: int, n_obj: int):
         self.popsize = popsize 
         self.n_obj = n_obj
 
-    def get_new_generation(self) -> Population:
+    def get_new_generation(self, 
+                           population: Population, 
+                           eval_func: FunctionType) -> Population:
         pass
 
     @abstractmethod
-    def get_offspring(self) -> Individual:
+    def get_offspring(self, 
+                      index: int, 
+                      population: Population, 
+                      eval_func: FunctionType) -> Individual:
         pass 
 
     def calc_fitness(self, population):
@@ -27,8 +35,7 @@ class Optimizer(ABC):
         self.normalize_option = option 
 
 
-
-class Solution_archive(list):
+class Solution_archive(object):
 
     def __init__(self, n_wvec, size):
         self.limit_size = size
