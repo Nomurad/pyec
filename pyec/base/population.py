5ab7986ba4ffe95302106df13324e4c6d7b62f9d
nomurad <nomura-seiya@ed.tmu.ac.jp>
2020-11-16 21:06:50 +0900

    modify

--- pyec/base/population.py
+++ pyec/base/population.py
@@ -2,6 +2,7 @@ from typing import List, overload
 
 from .indiv import Individual
 
+
 class PopulationError(Exception):
     """
     population class 用例外クラス
@@ -20,33 +21,33 @@ class Population(object):
             self.pop = pop 
             self.capacity = pop.capacity
             # self.current_id = 0
-        
+
         else:
             if indivs is not None:
-                self.pop:List[Individual] = indivs
+                self.pop: List[Individual] = indivs
             else:
-                self.pop:List[Individual] = []
+                self.pop: List[Individual] = []
 
             if capa is None:
                 raise Exception("You should set population capacity.")
             else:
                 self.capacity = capa
-                
-    def get_inviv(self, key:int) -> Individual:
+
+    def get_inviv(self, key: int) -> Individual:
         return self.pop[key]
 
     @overload
-    def __getitem__(self, key:int) -> Individual: ...
+    def __getitem__(self, key: int) -> Individual: ...
 
     @overload
-    def __getitem__(self, s:slice) -> List[Individual]: ...
+    def __getitem__(self, s: slice) -> List[Individual]: ...
 
     def __getitem__(self, key):
         return self.pop[key]
-    
-    def __setitem__(self, key:int, indiv:Individual):
+
+    def __setitem__(self, key: int, indiv: Individual):
         self.pop[key] = indiv
-    
+
     def __len__(self) -> int:
         return len(self.pop)
 
@@ -56,12 +57,12 @@ class Population(object):
         pop = self.pop + other.pop
         return Population(indivs=pop, capa=len(pop))
 
-    def append(self, indiv:Individual):
+    def append(self, indiv: Individual):
         if not isinstance(indiv, Individual):
             raise PopulationError("Only you can append indiv which class is Individual.")
         elif self.filled():
             raise PopulationError("This population is already filled")
-        
+
         self.pop.append(indiv)
 
     def sort(self, *args, **kwargs):
