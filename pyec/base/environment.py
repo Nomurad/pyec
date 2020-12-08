7723b8ca7da0cea6b5f114746022e0549fceb5c1
nomurad <nomura-seiya@ed.tmu.ac.jp>
2020-12-08 19:38:27 +0900

    modify

--- pyec/base/environment.py
+++ pyec/base/environment.py
@@ -102,7 +102,7 @@ class Environment(object):
         self.feasible_indivs_id: List[int] = []
 
         # initializerの設定
-        self.initializer = initializer(dv_size, popsize, n_obj)
+        self.initializer = initializer(dv_size, popsize=popsize, n_obj=n_obj)
         self.creator = Creator(self.initializer, self.pool)
 
     def alternate(self, population=None, indivs=None):
